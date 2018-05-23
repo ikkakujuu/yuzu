e8f8c1606c4b9e014c5aa70917db949b3d94dda1
Tim W <tim@weiss.moe>
2018-05-23 09:21:49 +0200

    🦄 Simple placeholder page

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,10 @@ module.exports = {
 				rel: 'icon',
 				type: 'image/x-icon',
 				href: '/favicon.ico'
+			},
+			{
+				rel: 'stylesheet',
+				href: 'https://fonts.googleapis.com/css?family=Barlow:400,500'
 			}
 		]
 	},
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <section>
+  <section class="center">
     <h1 class="header">Ikkakujū 🦄</h1>
+	<a href="https://github.com/ikkakujuu">github.com/ikkakujuu</a>
   </section>
 </template>
 
@@ -17,8 +18,22 @@ export default class extends Vue {
 }
 </script>
 <style scoped>
+.center {
+	display: flex;
+	align-items: center;
+	height: 100vh;
+	justify-content: center;
+	flex-direction: column;
+}
 .header {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana,
-    sans-serif;
+	font-family: "Barlow", Tahoma, Geneva, Verdana, sans-serif;
+	font-weight: 500;
+	font-size: 3rem;
+	margin-bottom: 0.5rem;
+}
+a {
+	font-family: "Barlow", Tahoma, Geneva, Verdana, sans-serif;
+	color: #333333;
+	text-decoration: none;
 }
 </style>
